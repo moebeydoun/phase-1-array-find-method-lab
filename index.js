12e29c88e61e8a7d9b5dde300e229a207ada52de
moe beydoun <moebeydoun24@yahoo.com>
2023-11-14 14:57:18 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function superbowlWin(resultsArray) {
+  // Use the find() method to search for a winning result ("W")
+  const winningResult = resultsArray.find(result => result.result === "W");
+
+  // Check if a win was found
+  if (winningResult) {
+    // If a win is found, return the corresponding year
+    return winningResult.year;
+  } else {
+    // If no win is found, return undefined
+    return undefined;
+  }
+}
